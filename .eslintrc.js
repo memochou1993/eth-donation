15a3156fd20ed16a9c6d9b702f4f76f53b9a7e3f
Memo Chou <memochou1993@gmail.com>
2022-03-19 03:07:04 +0800

    Add lint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-undef': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+};
